fd98a7b1c8273510f52422bf20e5c198e72366ca
jordanypb <jordanaladesuru@gmail.com>
2022-02-24 09:34:43 -0600

    new update

---
+++ other.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+	var greeting;
+	var element = document.getElementById("intro");
+
+	var button = document.querySelector("button");
+	button.addEventListener("click", addText);
+
+	//document.write('<h1> + greeting + '</h1>');
+	//element.insertAdjacentHTML ("afterbegin", "<h1>" + greeting + "</h1>");
+
+
+function addText(){
+	var textField = document.getElementById("textfield");
+	var msg = "<h1> This text was added!</h1>";
+	textField.innerHTML = msg;
+}
+
+}
+
