01d43125b7969a00a9f32f828798f79379a31b79
jordanypb <jordanaladesuru@gmail.com>
2022-04-13 18:46:39 -0500

    all my todo sstuff

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//SELECTORS
+//selectors
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
